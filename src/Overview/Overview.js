2674524fc340ce3723ea74fd342ddc8782e6bd79
hanka <hankainc@protonmail.com>
2019-11-21 19:07:46 +1030

    Balance, value and transaction log for supplied address

--- src/Overview/Overview.js
+++ src/Overview/Overview.js
@@ -3,20 +3,7 @@ import './Overview.css';
 
 function Overview() {
   return (
-    <div class='container'>
-      <div class='overview'>
-        <div class='row'>
-          <div class="col">
-            <div class='card' >
-              <div class="card-header">Overview</div>
-                <ul class="list-group list-group-flush">
-                  <li class="list-group-item" id='bal'>Balance: ... Ether</li>
-                  <li class="list-group-item" id='val'>Value: $...</li>
-                </ul>
-              </div>
-            </div>
-          </div>
-      </div>
+    <div>
     </div>
   );
 }
--- src/Transactions/Transactions.js
+++ src/Transactions/Transactions.js
@@ -2,44 +2,8 @@ import React from 'react';
 import './Transactions.css';
 
 function Transactions() {
-  const test = () => {
-    console.log('hello')
-  }
-
   return (
-    <div class='container'>
-      <div class='row'>
-        <div class="col">
-          <div class='card'>
-            <div class="card-header">Transactions</div>
-            <table class ='table table-hover view'>
-              <thead class='thead-light'>
-                <tr>
-                  <th scope="col">Date</th>
-                  <th scope="col">Block</th>
-                  <th scope="col">Txn #</th>
-                  <th scope="col">From</th>
-                  <th scope="col">To</th>
-                  <th scope="col">Amount</th>
-                  <th scope="col">Txn Fee</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td class="d-none d-sm-table-cell crop"></td>
-                  <td class="d-none d-sm-table-cell"></td>
-                  <td class="d-none d-sm-table-cell"></td>
-                  <td class="d-none d-sm-table-cell"></td>
-                  <td class="d-none d-sm-table-cell"></td>
-                  <td class="d-none d-sm-table-cell">Ether</td>
-                  <td class="d-none d-sm-table-cell"></td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-    </div>
+      <div></div>
   );
 }
 

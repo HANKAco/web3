9cc7bf41b44584e2e4496d70c363e314a590b8c6
hanka <hankainc@protonmail.com>
2020-01-04 19:34:57 +1030

    card changes to account create

--- src/Create/Create.js
+++ src/Create/Create.js
@@ -25,24 +25,22 @@ class Create extends Component {
         <div class="container">
           <div class="row">
             <div class="col-sm-10 form-group-lg">
-              <button type="button" Value='Send' class="btn btn-primary" onClick={()=>this.create()}>Create</button>
               <form>
                 <div class='card-body'>
                   <h5 class="card-title">Account</h5>
-                  <p>be aware this will generate a private key</p>
+                  <p class='card-text'>be aware this will generate a private key</p>
+                  <button type="button" Value='Send' class="btn btn-primary" onClick={()=>this.create()}>Create</button>
                   {this.state.acc.map((item) => {
                     return (
-                      <p>
-                        <ul>
-                          <label>Address</label>
-                          <li>{item.address}</li>
+                        <ul class="list-group">
+                          <label class="list-group-item list-header">Address</label>
+                          <li class="list-group-item">{item.address}</li>
                           <br></br>
-                          <label>Private Key</label>
-                          <li>{item.privateKey}</li>
+                          <label class="list-group-item list-header">Private Key (keep this secure)</label>
+                          <li class="list-group-item">{item.privateKey}</li>
                         </ul>
-                      </p>
-                    );
-                  })}
+                      );
+                    })}
                 </div>
               </form>
             </div>
